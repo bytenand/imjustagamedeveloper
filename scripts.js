f1ecdb736a60bc0967674364d8a2aae8fc593fa9
meta <164345121+weakmetatable@users.noreply.github.com>
2025-02-05 14:39:34 +0000

    Image Size Decrease

--- scripts.js
+++ scripts.js
@@ -1,28 +1,10 @@
 document.addEventListener("scroll", function () {
     let scrollY = window.scrollY;
 
-    
-    document.querySelector(".Foreground2").style.transform = `translateY(${scrollY * 0.35}px)`;
-    document.querySelector(".Foreground3").style.transform = `translateY(${scrollY * 0.45}px)`;
+    document.querySelector(".ForegroundRocks").style.transform = `translateY(${scrollY * 0.35}px)`;
+    document.querySelector(".BackgroundRocks").style.transform = `translateY(${scrollY * 0.45}px)`;
     document.querySelector(".Skyscape").style.transform = `translateY(${scrollY * 0.5}px)`;
     document.querySelector(".TopClouds").style.transform = `translateY(${scrollY * 0.85}px)`;
     document.querySelector(".BottomClouds").style.transform = `translateY(${scrollY * 0.75}px)`;
     document.querySelector(".Mountains").style.transform = `translateY(${scrollY * 0.4}px)`;
-});
-
-let cloudOffset = 0; // Start position
-
-function animateClouds() {
-    cloudOffset -= 0.05; // Adjust speed (negative moves left)
-    
-
-    // Reset position when clouds move too far left to create a loop
-    if (cloudOffset < -window.innerWidth) {
-        cloudOffset = window.innerWidth;
-    }
-
-    requestAnimationFrame(animateClouds);
-}
-
-// Start animation
-animateClouds();
+}, { passive: true });
