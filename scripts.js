6c85a4bac7e5da9eeb71d9f3ae25f7cf05e24e42
meta <164345121+weakmetatable@users.noreply.github.com>
2025-02-06 09:36:18 +0000

    e

--- scripts.js
+++ scripts.js
@@ -1,12 +1,19 @@
+function clamp(x, min, max) {
+    return Math.max(min, Math.min(x, max));
+}
+
 document.addEventListener("scroll", function () {
     let scrollY = window.scrollY;
+    
+    let scrollHeight = document.documentElement.scrollHeight
+    let allowedScrollHeight = scrollHeight / 4
 
-    document.querySelector(".ForegroundRocks").style.transform = `translateY(${scrollY * 0.35}px)`;
-    document.querySelector(".BackgroundRocks").style.transform = `translateY(${scrollY * 0.45}px)`;
-    document.querySelector(".Skyscape").style.transform = `translateY(${scrollY * 0.5}px)`;
-    document.querySelector(".TopClouds").style.transform = `translateY(${scrollY * 0.85}px)`;
-    document.querySelector(".BottomClouds").style.transform = `translateY(${scrollY * 0.75}px)`;
-    document.querySelector(".Mountains").style.transform = `translateY(${scrollY * 0.4}px)`;
-    document.querySelector(".Sun").style.transform = `translateY(${scrollY * 0.6}px)`;
-    document.querySelector(".Skyscape").style.transform = `translateY(${scrollY * 0.4}px)`;
+    document.querySelector(".ForegroundRocks").style.transform = `translateY(${clamp(scrollY * 0.35, 0, allowedScrollHeight)}px)`;
+    document.querySelector(".BackgroundRocks").style.transform = `translateY(${clamp(scrollY * 0.45, 0, allowedScrollHeight)}px)`;
+    document.querySelector(".Skyscape").style.transform = `translateY(${clamp(scrollY * 0.5, 0, allowedScrollHeight)}px)`;
+    document.querySelector(".TopClouds").style.transform = `translateY(${clamp(scrollY * 0.85, 0, allowedScrollHeight)}px)`;
+    document.querySelector(".BottomClouds").style.transform = `translateY(${clamp(scrollY * 0.75, 0, allowedScrollHeight)}px)`;
+    document.querySelector(".Mountains").style.transform = `translateY(${clamp(scrollY * 0.4, 0, allowedScrollHeight)}px)`;
+    document.querySelector(".Sun").style.transform = `translateY(${clamp(scrollY * 0.6, 0, allowedScrollHeight)}px)`;
+    document.querySelector(".Skyscape").style.transform = `translateY(${clamp(scrollY * 0.4, 0, allowedScrollHeight)}px)`;
 }, { passive: true });
