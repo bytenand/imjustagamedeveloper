1b363a63d15bfc86fce770c261cf8e609c67f935
meta <164345121+weakmetatable@users.noreply.github.com>
2025-02-06 05:40:01 +0000

    d

--- scripts.js
+++ scripts.js
@@ -7,4 +7,6 @@ document.addEventListener("scroll", function () {
     document.querySelector(".TopClouds").style.transform = `translateY(${scrollY * 0.85}px)`;
     document.querySelector(".BottomClouds").style.transform = `translateY(${scrollY * 0.75}px)`;
     document.querySelector(".Mountains").style.transform = `translateY(${scrollY * 0.4}px)`;
+    document.querySelector(".Sun").style.transform = `translateY(${scrollY * 0.6}px)`;
+    document.querySelector(".Skyscape").style.transform = `translateY(${scrollY * 0.4}px)`;
 }, { passive: true });
