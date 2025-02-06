b7ebbd46382b4c556488ab9eac0ea43f123f7ba0
meta <164345121+weakmetatable@users.noreply.github.com>
2025-02-06 19:23:15 +0000

    Testing index

---
+++ myuser/scripts.js
@@ -0,0 +1,42 @@
+function clamp(x, min, max) {
+    return Math.max(min, Math.min(x, max));
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.addEventListener("scroll", function () {
+        let scrollY = window.scrollY;
+        
+        let scrollHeight = document.documentElement.scrollHeight
+        let allowedScrollHeight = scrollHeight / 3
+    
+        document.querySelector(".ForegroundRocks").style.transform = `translateY(${clamp(scrollY * 0.35, 0, allowedScrollHeight)}px)`;
+        document.querySelector(".BackgroundRocks").style.transform = `translateY(${clamp(scrollY * 0.45, 0, allowedScrollHeight)}px)`;
+        document.querySelector(".Skyscape").style.transform = `translateY(${clamp(scrollY * 0.5, 0, allowedScrollHeight)}px)`;
+        document.querySelector(".TopClouds").style.transform = `translateY(${clamp(scrollY * 0.85, 0, allowedScrollHeight)}px)`;
+        document.querySelector(".BottomClouds").style.transform = `translateY(${clamp(scrollY * 0.75, 0, allowedScrollHeight)}px)`;
+        document.querySelector(".Mountains").style.transform = `translateY(${clamp(scrollY * 0.4, 0, allowedScrollHeight)}px)`;
+        document.querySelector(".Sun").style.transform = `translateY(${clamp(scrollY * 0.6, 0, allowedScrollHeight)}px)`;
+        document.querySelector(".Skyscape").style.transform = `translateY(${clamp(scrollY * 0.4, 0, allowedScrollHeight)}px)`;
+    }, { passive: true });
+
+    function adjustMargins() {
+        let aboutMeContainer = document.querySelector(".AboutMeContainer");
+
+        if (!aboutMeContainer) {
+            console.error("Element with class 'AboutMeContainer' not found!");
+            return;
+        }
+
+        if (window.innerWidth < 1362) {
+            aboutMeContainer.style.marginLeft = "5%";
+            aboutMeContainer.style.marginRight = "5%";
+        } else {
+            aboutMeContainer.style.marginLeft = "20%";
+            aboutMeContainer.style.marginRight = "20%";
+        }
+    }
+
+    adjustMargins();
+
+    window.addEventListener("resize", adjustMargins);
+});
