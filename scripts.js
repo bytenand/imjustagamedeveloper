552396d43fa42b85a23aa385e08b03c3f7fbdc13
meta <164345121+weakmetatable@users.noreply.github.com>
2025-02-07 18:40:38 +0000

    g

--- scripts.js
+++ scripts.js
@@ -3,11 +3,14 @@ function clamp(x, min, max) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+    const parallaxClasses = [".JustADevLabel", ".DeveloperLabel", ".FullstackGameLabel", ".AboutMeText", ".HeaderText", ".LongAboutMeText", ".SkillButton"];
+    const stopPoint = window.innerHeight;
+    
     document.addEventListener("scroll", function () {
         let scrollY = window.scrollY;
         
-        let scrollHeight = document.documentElement.scrollHeight
-        let allowedScrollHeight = scrollHeight / 3
+        let scrollHeight = document.documentElement.scrollHeight;
+        let allowedScrollHeight = scrollHeight / 3;
     
         document.querySelector(".ForegroundRocks").style.transform = `translateY(${clamp(scrollY * 0.35, 0, allowedScrollHeight)}px)`;
         document.querySelector(".BackgroundRocks").style.transform = `translateY(${clamp(scrollY * 0.45, 0, allowedScrollHeight)}px)`;
@@ -17,6 +20,23 @@ document.addEventListener("DOMContentLoaded", () => {
         document.querySelector(".Mountains").style.transform = `translateY(${clamp(scrollY * 0.4, 0, allowedScrollHeight)}px)`;
         document.querySelector(".Sun").style.transform = `translateY(${clamp(scrollY * 0.6, 0, allowedScrollHeight)}px)`;
         document.querySelector(".Skyscape").style.transform = `translateY(${clamp(scrollY * 0.4, 0, allowedScrollHeight)}px)`;
+
+        parallaxClasses.forEach(className => {
+            document.querySelectorAll(className).forEach(element => {
+                let elementRect = element.getBoundingClientRect();
+                let elementTop = elementRect.top;
+                
+                if (elementTop < stopPoint) { 
+                    let moveAmount = Math.min((stopPoint - elementTop) * 0.4, stopPoint / 8);
+                    let opacityAmount = Math.min((stopPoint - elementTop) / stopPoint * 4, 1);
+
+                    element.style.transform = `translateY(-${moveAmount}px)`;
+                    element.style.opacity = opacityAmount;
+                }
+            });
+        });
+
+
     }, { passive: true });
 
     function adjustMargins() {
@@ -31,8 +51,8 @@ document.addEventListener("DOMContentLoaded", () => {
             aboutMeContainer.style.marginLeft = "5%";
             aboutMeContainer.style.marginRight = "5%";
         } else {
-            aboutMeContainer.style.marginLeft = "20%";
-            aboutMeContainer.style.marginRight = "20%";
+            aboutMeContainer.style.marginLeft = "15%";
+            aboutMeContainer.style.marginRight = "15%";
         }
     }
 
